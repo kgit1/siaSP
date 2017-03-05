5479c3a87a87889202f30c719b7a925b33d05406
kgit1 <kgit1@rambler.ru>
2017-03-05 18:16:16 +0200

    test db connection and hibernate some queries

--- aSpitter.dynamicWebProject/src/com/k1/spitter/checkdb/DbTestHibernate.java
+++ aSpitter.dynamicWebProject/src/com/k1/spitter/checkdb/DbTestHibernate.java
@@ -1,5 +1,7 @@
 package com.k1.spitter.checkdb;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -31,6 +33,38 @@ public class DbTestHibernate {
 		// create session
 		Session session = factory.getCurrentSession();
 
+		// start transaction
+		session.beginTransaction();
+
+		// remember in query quotes in java - field name must equals to name in
+		// entity, not table, hibernate will automatically remap field - using
+		// @Column name value
+		// select all from table,
+		// equals SQL "select * from Spitter"
+		List<Spitter> spittersAll = session.createQuery("from Spitter").getResultList();
+		// select all with username "habuma",
+		// equals SQL "select * from spitter where username='habuma'"
+		List<Spitter> spittersByName = session.createQuery("from Spitter s where s.userName='habuma'").getResultList();
+		// select all with username "habuma",
+		// equals SQL "select * from spitter where email='habuma'"
+		List<Spitter> spittersByEmail = session.createQuery("from Spitter s where s.email='artnames@habuma.com'")
+				.getResultList();
+
+		// don't forget to close factory at the end
+		// spring will take on himself handling session open and close, but when
+		// we using poor hibernate
+		// better way to wrap all it try{}finally{factory.close();} block
+		factory.close();
+
+		for (Spitter spitter : spittersAll) {
+			System.out.println(spitter);
+		}
+		for (Spitter spitter : spittersByName) {
+			System.out.println(spitter);
+		}
+		for (Spitter spitter : spittersByEmail) {
+			System.out.println(spitter);
+		}
 	}
 
 }
