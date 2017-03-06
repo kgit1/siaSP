3b4425ee6574ca92423443331ad226ed5813bc81
kgit1 <kgit1@rambler.ru>
2017-03-06 21:03:25 +0200

    insert hibernate

--- aSpitter.dynamicWebProject/src/com/k1/spitter/checkdb/DbTestHibernate.java
+++ aSpitter.dynamicWebProject/src/com/k1/spitter/checkdb/DbTestHibernate.java
@@ -36,6 +36,7 @@ public class DbTestHibernate {
 		// start transaction
 		session.beginTransaction();
 
+		/////////////////////// SELECT/////////////////////////////
 		// remember in query quotes in java - field name must equals to name in
 		// entity, not table, hibernate will automatically remap field - using
 		// @Column name value
@@ -49,6 +50,22 @@ public class DbTestHibernate {
 		// equals SQL "select * from spitter where email='habuma'"
 		List<Spitter> spittersByEmail = session.createQuery("from Spitter s where s.email='artnames@habuma.com'")
 				.getResultList();
+
+		/////////////////////// INSERT/////////////////////////////
+		// (username, password, fullname, email, update_by_email) values
+		// ('habuma', 'password', 'Craig Walls', 'craig@habuma.com', false)
+
+//		session.beginTransaction();
+		Spitter spitter1 = new Spitter("paul", "qwerty", "donaldson", "donaldson@yahoo.com", false);
+		Spitter spitter2 = new Spitter("paul", "qwerty", "benson", "benson@yahoo.com", false);
+		Spitter spitter3 = new Spitter("iren", "qwerty", "donaldson", "iren@yahoo.com", false);
+		session.save(spitter1);
+		session.getTransaction().commit();
+		session = factory.getCurrentSession();
+		session.beginTransaction();
+		session.save(spitter2);
+		session.save(spitter3);
+		session.getTransaction().commit();
 
 		// don't forget to close factory at the end
 		// spring will take on himself handling session open and close, but when
