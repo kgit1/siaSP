69d113ed1f74f4d89634e956fe7c590fd957fe9b
kgit1 <kgit1@rambler.ru>
2017-03-19 00:17:38 +0200

    up

--- aSpitter.dynamicWebProject/src/com/k1/spitter/controller/HomeController.java
+++ aSpitter.dynamicWebProject/src/com/k1/spitter/controller/HomeController.java
@@ -2,13 +2,17 @@ package com.k1.spitter.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -83,6 +87,27 @@ public class HomeController {
 		theModel.addAttribute("user", getPrincipal());
 		return "utility/admin";
 	}
+	
+	@GetMapping("/dba")
+    public String dbaPage(Model theModel) {
+        theModel.addAttribute("user", getPrincipal());
+        return "utility/dba";
+    }
+ 
+	@GetMapping("/logout")
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "home";
+	}
+
+	@GetMapping("/Access_Denied")
+	public String accessDeniedPage(Model theModel) {
+		theModel.addAttribute("user", getPrincipal());
+		return "utility/accessDenied";
+	}
 
 	private List<Spitter> listSpitters() {
 		Session session = factory.getCurrentSession();
@@ -117,7 +142,6 @@ public class HomeController {
 		}
 		return userName;
 	}
-
 }
 
 // 1. HQL Select Query Example
